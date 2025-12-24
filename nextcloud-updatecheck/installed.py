672c0e400a07d46aa170d219b5e857718195c04d
Bernard Spil <brnrd@FreeBSD.org>
2025-12-24 14:31:10 +0100

    nextcloud-updatecheck: Fix Nextcloud version extraction from installed dir
    and add some logging while here.

--- nextcloud-updatecheck/installed.py
+++ nextcloud-updatecheck/installed.py
@@ -1,9 +1,12 @@
 """Extract apps information from a Nextcloud install"""
 
 import json
+import logging
 
 import xml.etree.ElementTree as ET
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 def get_installed_nextcloud_version(nextcloud_dir: Path) -> dict:
     """Extract Nextcloud version from installation
@@ -17,14 +20,17 @@ def get_installed_nextcloud_version(nextcloud_dir: Path) -> dict:
     version = {}
     with (nextcloud_dir / "version.php").open(encoding="utf-8") as f:
         for line in f:
-            if line[:3] in "$OC_":
+            line = line.removeprefix("$OC_")
+            parts = line.split("=")
+            if len(parts) == 1:
                 continue
-            parts = line[4:].split("=")
-            key = parts[0].strip(" ';")
-            value = parts[1].strip(" ';")
+            key = parts[0].strip(" ';\n")
+            value = parts[1].strip(" ';\n")
             if key == "VersionCanBeUpgradedFrom":
                 continue
             version[key] = value
+    logger.debug("get_installed_nextcloud_version: Extracted version %s from %s",
+                 version, nextcloud_dir / "version.php")
     return version
 
 def get_shipped_apps(nextcloud_dir):
@@ -61,5 +67,7 @@ def get_installed_apps(nextcloud_dir):
         port["version"] = root.find("version").text
         if port["name"] not in shipped_apps:
             ports.append(port)
+    logger.debug("get_installed_apps: Extracted apps %s from %s",
+                 ports, nextcloud_dir)
     return ports
 
